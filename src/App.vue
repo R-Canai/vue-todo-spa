2ac0559d2c61c432d81559104c1a0a8e64108ccd
R-Canai <canai3jp@gmail.com>
2017-12-17 20:04:25 +0900

    Add vuetifyjs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app id="inspire" light>
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="testMethod">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      drawer: null
+    }
+  },
+  methods: {
+    testMethod () {
+      alert('test ...')
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-
+@import "vuetify/dist/vuetify.min.css";
 </style>
