f0a82f190bb393b51037a093b2042219246d44cc
R-Canai <canai3jp@gmail.com>
2017-12-29 16:29:09 +0900

    [reset] remove all django project

--- django_rest_api/task/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .views import UserViewSet, TaskViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'tasks', TaskViewSet)
